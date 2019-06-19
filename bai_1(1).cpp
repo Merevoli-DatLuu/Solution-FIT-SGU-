a2a82135375d2a63c7223463cdf3c6676bf77632
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2019-06-19 15:10:36 +0700

    Create bai_1(1).cpp

---
+++ bai_1(1).cpp
@@ -0,0 +1,52 @@
+// Time: O(n*loglogn)
+// Space: O(n)
+// Sàng Nguyên tố Eratosthenes
+// Chạy Tốt Với n = 10^7
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+int n;
+bool *Sieve;
+vector <int> Primes;
+
+void Nhap(){
+    cin >> n;
+}
+
+void SieveofEratosthenes(){
+    Sieve = new bool[n+1];
+    for (int i=1; i<=n; i++){
+        Sieve[i] = true;
+    }
+    Sieve[0] = false;
+    Sieve[1] = false;
+    for (int i=2; i*i <= n; i++){
+        if (Sieve[i]){
+            for (int j= i*i; j<=n; j+=i){
+                Sieve[j] = false;
+            }
+        }
+    }
+    for (int i=2; i<=n; i++){
+        if (Sieve[i]){
+            Primes.push_back(i);
+        }
+    }
+    int res_1 = Primes.size();
+    cout << res_1 << "\n";
+    int res_2 = 0;
+    for (int i=1; i<res_1-1; i++){
+        if (Primes[i]*2 == Primes[i-1] + Primes[i+1]){
+            res_2++;
+        }
+    }
+    cout << res_2;
+}
+
+int main(){
+    Nhap();
+    SieveofEratosthenes();
+}
