6fbdada7483e1d4157cd2be012d79b0774f35e53
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2019-07-26 02:16:21 +0700

    Create bai_2_py.py

---
+++ Solution/Buổi 1/bai_2_py.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = [int(s) for s in input().split(' ')]
+
+a.sort()
+res = 0x3f3f3f3f
+for i in range(n - 1):
+    if (a[i + 1] - a[i] < res):
+        res = a[i + 1] - a[i]
+        
+print(res)
