9f20055da9cb8ec3641a2798b6ac14d45101cf14
Merevoli-DatLuu <47155364+Merevoli-DatLuu@users.noreply.github.com>
2019-07-26 02:03:04 +0700

    Create bai_1_py.cpp

---
+++ Solution/Buổi 1/bai_1_py.cpp
@@ -0,0 +1,19 @@
+n = int(input())
+a = [int(s) for s in input().split(' ')]
+a.insert(0, -0x3f3f3f3f)
+L = []
+L.append(0)
+
+res = 0
+
+for i in range(n):
+    max_L = 0;
+    for j in range(i+1):
+        if (a[i + 1] >= a[i - j] and max_L < L[i - j]):
+            max_L = L[i - j]
+    L.append(max_L + 1)
+    if (res < L[i + 1]):
+        res = L[i + 1]
+
+print(res)
+
